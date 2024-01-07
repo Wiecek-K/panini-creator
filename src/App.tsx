6335c5884529ead0e8a526006f7c55ff4545f8b5
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-01-07 04:31:45 +0100

    added rings animations
    pls clean this code
    and add rest of animations to the main bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import './App.css'
+import { useState } from 'react'
 import { SplashScreen } from './components/SplashScreen/SplashScreen'
 
 function App() {
+  const [appState, setAppState] = useState<'start' | 'form' | 'end'>('start')
+  const [isClosing, setIsClosing] = useState(false)
+
+  const handleOpenForm = () => {
+    // setAppState('form')
+    setIsClosing((prev) => !prev)
+  }
+
   return (
     <>
-      <SplashScreen
-        header="Panini Creator"
-        btnText="BEGIN"
-        btnFnc={() => {
-          null
-        }}
-      />
+      {appState === 'start' ? (
+        <SplashScreen
+          header="Panini Creator"
+          btnText="BEGIN"
+          btnFnc={() => {
+            handleOpenForm()
+          }}
+          isClosing={isClosing}
+        />
+      ) : null}
+      {appState === 'form' ? 'FORM' : null}
+      {appState === 'end' ? 'END' : null}
     </>
   )
 }
--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -3,30 +3,70 @@ import styles from './SplashScreen.module.css'
 interface SplashScreenProps {
   header: string
   btnText: string
+  isClosing?: boolean
   btnFnc(): void
 }
 
 export const SplashScreen = ({
   header,
   btnText,
+  isClosing = false,
   btnFnc,
 }: SplashScreenProps) => {
   return (
     <>
-      <div className={styles.ringLeft} />
-      <div className={styles.ringHalfLeft} />
-      <div className={styles.ringHalfRight} />
-      <div className={styles.ringRight} />
-      <div className={styles.ringTop} />
-      <div className={styles.ringBottom} />
+      <div
+        className={`${styles.ringLeft} ${isClosing ? styles.moveRingLeft : ''}`}
+      />
+      <div
+        className={`${styles.ringHalfLeft} ${
+          isClosing ? styles.moveRingHalfLeft : ''
+        }`}
+      />
+      <div
+        className={`${styles.ringHalfRight} ${
+          isClosing ? styles.moveRingHalfRight : ''
+        }`}
+      />
+      <div
+        className={`${styles.ringRight} ${
+          isClosing ? styles.moveRingRight : ''
+        }`}
+      />
+      <div
+        className={`${styles.ringTop} ${isClosing ? styles.moveRingTop : ''}`}
+      />
+      <div
+        className={`${styles.ringBottom} ${
+          isClosing ? styles.moveRingBottom : ''
+        }`}
+      />
 
-      <div className={styles.ringCenter}>
-        <div className={styles.mainBar}>
-          <div className={styles.splashScreenText}>
-            <h1>{header}</h1>
-            <button onClick={btnFnc}>{btnText}</button>
-          </div>
-        </div>
+      <div
+        className={`${styles.ringCenterStatic} ${
+          isClosing ? styles.isClosing : ''
+        }`}
+      >
+        <div
+          className={`${styles.mainBar} ${isClosing ? styles.moveMainBar : ''}`}
+        />
+      </div>
+
+      <div
+        className={`${styles.ringCenterAnimated} ${
+          isClosing ? styles.isClosing : ''
+        }`}
+      ></div>
+      <div className={styles.splashScreenText}>
+        <h1 className={isClosing ? '' : styles.textBackground}>{header}</h1>
+        <button
+          className={`${isClosing ? '' : styles.textBackground} ${
+            styles.button
+          }`}
+          onClick={btnFnc}
+        >
+          {btnText}
+        </button>
       </div>
     </>
   )
